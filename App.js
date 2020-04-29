82741f789f8e13d312abbcbcefc143e3b386100e
Sandro Torres <sandrotorres.ti@gmail.com>
2020-04-29 05:28:38 -0300

    Tela  Padrao pronta

--- App.js
+++ App.js
@@ -11,41 +11,88 @@ import {
 
 export default function App() {
   return (
-    <KeyboardAvoidingView style={styles.container}>
-    <View>
-      <Image source={require('./src/assets/logo.png')} />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+		<View style={styles.containerLogo}>
+			<Image source={require('./src/assets/logo.png')} />
+		</View>
 
-    <View>
-      <TextInput
-        placeholder="E-mail"
-        autoCorrect={false}
-        onChangeText={() => {}}
-	  />
-	  
-      <TextInput
-        placeholder="Senha"
-        autoCorrect={false}
-        onChangeText={() => {}}
-	  />
-	  
-	  <TouchableOpacity>
-	 	<Text>Acessar</Text> 
-	  </TouchableOpacity>
+		<View style={styles.container}>
+			<TextInput
+				style={styles.input}
+				placeholder="E-mail"
+				autoCorrect={false}
+				onChangeText={() => {}}
+			/>
+			
+			<TextInput
+				style={styles.input}
+				placeholder="Senha"
+				autoCorrect={false}
+				onChangeText={() => {}}
+			/>
+			
+			<TouchableOpacity style={styles.btnSubmit}>
+				<Text style={styles.submitText}>Acessar</Text> 
+			</TouchableOpacity>
 
-	  <TouchableOpacity>
-	 	<Text>Criar conta gratuita</Text> 
-	  </TouchableOpacity>
-    </View>
+			<TouchableOpacity style={styles.btnRegister}>
+				<Text style={styles.registerText}>Criar conta gratuita</Text> 
+			</TouchableOpacity>
+		</View>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+	background: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLogo: {
+	flex: 1,
+	justifyContent: 'center'
+  },
+  container: {
+	  flex: 1,
+	  alignItems: 'center',
+	  justifyContent: 'center',
+		width: '90%',
+		paddingBottom: 50
+  },
+  input: {
+	   backgroundColor: '#FFF',
+	   width: '90%',
+	   marginBottom: 15,
+	   color: '#E45126',
+	   fontSize: 17,
+	   borderRadius: 7,
+	   padding: 10,
+	},
+	btnSubmit: {
+		backgroundColor: '#0c73b8',
+		width: '90%',
+		height: 45,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 7
+	},
+	submitText: {
+		color: '#FFF',
+		fontSize: 18,
+		fontWeight: 'bold'
+	},
+	btnRegister: {
+		width: '60%',
+		height: 45,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 10,
+		borderRadius: 7
+	},
+	registerText: {
+		color: '#FFF',
+		fontSize: 16,
+	}
 });
